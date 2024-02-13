2de068c69fe35657ce2a9709cae3a60d2fe1c077
SpartQS <rebrikov.vyacheslav@gmail.com>
2024-02-13 16:42:02 +0300

    task_7

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,10 +4,11 @@ import './App.css';
 import React from 'react';
 
 function App() {
-	return <div>
-		text
+	return <div className="o">
+		nah, i'd, win!
+	<p className="t">Satoru</p>
+    <p className="g">Gojo</p>
 	</div>;
 }
-
 
 export default App;
