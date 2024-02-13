755259f7d227e71d9674a60ef4bdd42392db04f8
SpartQS <rebrikov.vyacheslav@gmail.com>
2024-02-13 16:47:10 +0300

    task_8

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,10 +4,19 @@ import './App.css';
 import React from 'react';
 
 function App() {
-	return <div className="o">
-		nah, i'd, win!
-	<p className="t">Satoru</p>
-    <p className="g">Gojo</p>
+	return <div>
+		<ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+    <li>5</li>
+    <li>6</li>
+    <li>7</li>
+    <li>8</li>
+    <li>9</li>
+    <li>10</li>
+		</ul>
 	</div>;
 }
 
