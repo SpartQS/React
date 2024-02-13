fbbf2151f504df08b7233b35e2ba63a7449824eb
SpartQS <rebrikov.vyacheslav@gmail.com>
2024-02-13 18:01:56 +0300

    task_16

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,16 +4,14 @@ import './App.css';
 import React from 'react';
 
 function App() {
-	const arr = [1, 2, 3, 4, 5];
+	const obj = {name: 'john', surname: 'smit'};
 	
 	return <div>
-		<ul>
-		<li>{arr[0]}</li>
-		<li>{arr[1]}</li>
-		<li>{arr[2]}</li>
-		<li>{arr[3]}</li>
-		<li>{arr[4]}</li>
-		</ul>
+		<p>
+	name:    <span>{obj.name}</span></p>
+	<p>
+	surname: <span>{obj.surname}</span>
+	</p>
 	</div>;
 }
 
