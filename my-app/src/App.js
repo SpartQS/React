b6a4a3de2ff1201eece5781c642d4603fa4ca419
SpartQS <rebrikov.vyacheslav@gmail.com>
2024-02-19 20:58:06 +0300

    task_23.2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,24 +4,30 @@ import './App.css';
 import React from 'react';
 
 function App() {
-	return <table>
-		<tbody>
-    <tr>
-      <ul>
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
-    <li>4</li>
-    <li>5</li>
-    <li>6</li>
-    <li>7</li>
-    <li>8</li>
-    <li>9</li>
-    <li>10</li>
-		</ul>
-    </tr>
-		</tbody>
-	</table>;
+  return (
+    <div>
+      <table>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>5</td>
+            <td>6</td>
+          </tr>
+          <tr>
+            <td>7</td>
+            <td>8</td>
+            <td>9</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
+
 
 export default App;
