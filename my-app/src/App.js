3570fa85041b79e74e80d376714e47ce77e656aa
SpartQS <rebrikov.vyacheslav@gmail.com>
2024-02-27 16:20:03 +0300

    task_36

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,18 +3,28 @@ import './App.css';
 
 import React from 'react';
 
-function App() {
-  function handleClick(event) {
-    console.log(event.target);
+/*function App() {
+  function func(arg, event) {
+    console.log(arg, event);
   }
-
+  
   return (
     <div>
-      <button onClick={handleClick}>Click me</button>
+      <button onClick={(event) => func('eee', event)}>act</button>
     </div>
   );
+  }*/
+
+  function App() {
+    function func(event, arg) {
+      console.log(event, arg);
+    }
+    
+    return (
+      <div>
+        <button onClick={(event) => func(event, 'eee')}>act</button>
+      </div>
+    );
   }
-
-
 
 export default App;
