060f6c95a22e364ebd4d72b9fdda9587be1eaa8b
SpartQS <rebrikov.vyacheslav@gmail.com>
2024-02-27 15:57:23 +0300

    task_34

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,18 +4,15 @@ import './App.css';
 import React from 'react';
 
 function App() {
-    function show1() {
-      alert(1);
-    }
-    
-    function show2() {
-      alert(2);
-    }
-    
-    return <div>
-      <button onClick={show1}>act1</button>
-      <button onClick={show2}>act2</button>
-    </div>;
+  function showMess(text) {
+		alert(text);
+	}
+	
+	return <div>
+		<button onClick={() => showMess('1')}>show1</button>
+		<button onClick={() => showMess('2')}>show2</button>
+    <button onClick={() => showMess('3')}>show3</button>
+	</div>;
   }
 
 
