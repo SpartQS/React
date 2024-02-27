a906f53ad44eb46adc3e5ff09fee1cdfcd8cf12a
SpartQS <rebrikov.vyacheslav@gmail.com>
2024-02-27 16:40:48 +0300

    task_42

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,14 +5,14 @@ import React from 'react';
 
 function App() {
   const users = [
-    { name: 'user1', surn: 'surn1', age: 30 },
-    { name: 'user2', surn: 'surn2', age: 31 },
-    { name: 'user3', surn: 'surn3', age: 32 },
+    { id: 1, name: 'user1', surn: 'surn1', age: 30 },
+    { id: 2, name: 'user2', surn: 'surn2', age: 31 },
+    { id: 3, name: 'user3', surn: 'surn3', age: 32 },
   ];
 
-  const userList = users.map(function (user, index) {
+  const userList = users.map(function (user) {
     return (
-      <li key={index}>
+      <li key={user.id}>
         <span>{user.name}</span>:
         <span>{user.surn}</span>,
         <span>{user.age}</span>
@@ -22,9 +22,5 @@ function App() {
 
   return <ul>{userList}</ul>;
 }
-
-
-
-
 
 export default App;
