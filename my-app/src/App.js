0084861784c7e91c16996bca5410be1fc097bb19
SpartQS <rebrikov.vyacheslav@gmail.com>
2024-03-05 17:40:46 +0300

    task_52

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,30 +1,24 @@
-import React from 'react';
-import { nanoid } from 'nanoid';
+import React, { useState } from 'react';
 
 function App() {
-  function id() {
-    return nanoid();
-  }
-
-  const users = [
-    { id: id(), name: 'User1', age: 25 },
-    { id: id(), name: 'User2', age: 30 },
-    { id: id(), name: 'User3', age: 22 },
-  ];
+  const [firstName, setFirstName] = useState('John');
+  const [lastName, setLastName] = useState('Doe');
+  const [age, setAge] = useState(30);
 
   return (
     <div>
-      <h2>Информация о пользователях:</h2>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            <p>ID: {user.id}</p>
-            <p>Name: {user.name}</p>
-            <p>Age: {user.age}</p>
-            <hr />
-          </li>
-        ))}
-      </ul>
+      <div>
+        <label>Имя: </label>
+        <span>{firstName}</span>
+      </div>
+      <div>
+        <label>Фамилия: </label>
+        <span>{lastName}</span>
+      </div>
+      <div>
+        <label>Возраст: </label>
+        <span>{age}</span>
+      </div>
     </div>
   );
 }
