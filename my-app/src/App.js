5905c1eb2e27d67f1bb6e740c9671fbad9fbd6b8
SpartQS <rebrikov.vyacheslav@gmail.com>
2024-03-11 16:45:47 +0300

    task_56

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,29 @@
 import React, { useState } from 'react';
 
 function App() {
-  const [age, setAge] = useState(0);
+  const [text1, setText1] = useState('');
+  const [text2, setText2] = useState('');
+
+  function handleChangeInput1(event) {
+    setText1(event.target.value);
+  }
+
+  function handleChangeInput2(event) {
+    setText2(event.target.value);
+  }
 
   return (
     <div>
-      <p>Возраст: {age}</p>
-      <button onClick={() => setAge(age + 1)}>Увеличить возраст</button>
-      <button onClick={() => setAge(age - 1)}>Уменьшить возраст</button>
+      <label>Input 1: </label>
+      <input value={text1} onChange={handleChangeInput1} />
+      <p>Text from Input 1: {text1}</p>
+
+      <label>Input 2: </label>
+      <input value={text2} onChange={handleChangeInput2} />
+      <p>Text from Input 2: {text2}</p>
     </div>
   );
 }
 
 export default App;
+
