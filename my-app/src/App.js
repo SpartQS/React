3c82cd2fe5dc862b5db2d8c98709d3f7d2d8cfd7
SpartQS <rebrikov.vyacheslav@gmail.com>
2024-03-11 17:37:48 +0300

    task_64

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,45 +1,24 @@
 import React, { useState } from 'react';
 
 function App() {
-  const [isOver18, setIsOver18] = useState(false);
+  const [selectedCity, setSelectedCity] = useState('');
 
-  let content;
-  if (isOver18) {
-    content = (
-      <div>
-        <h2>Ура, вам уже есть 18</h2>
-        <p>здесь расположен контент только для взрослых</p>
-      </div>
-    );
-  } else {
-    content = (
-      <div>
-        <p>увы, вам еще нет 18 лет:(</p>
-      </div>
-    );
+  function handleChange(event) {
+    setSelectedCity(event.target.value);
   }
 
   return (
     <div>
-      <input type="checkbox" checked={isOver18} onChange={() => setIsOver18(!isOver18)} />
-      {content}
+      <select value={selectedCity} onChange={handleChange}>
+        <option value="">Выберите город</option>
+        <option value="moscow">Москва</option>
+        <option value="london">Лондон</option>
+        <option value="new-york">Нью-Йорк</option>
+        <option value="tokyo">Токио</option>
+      </select>
+      <p>Ваш выбор: {selectedCity || 'не выбран'}</p>
     </div>
   );
 }
 
 export default App;
-
-/*import React, { useState } from 'react';
-
-function App() {
-  const [isVisible, setIsVisible] = useState(false);
-
-  return (
-    <div>
-      <input type="checkbox" checked={isVisible} onChange={() => setIsVisible(!isVisible)} />
-      {isVisible && <p>Этот абзац видимый</p>}
-    </div>
-  );
-}
-
-export default App;*/
