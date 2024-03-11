ff2fb61d3102dae3923d9182ba3fb2436e310f5e
SpartQS <rebrikov.vyacheslav@gmail.com>
2024-03-11 17:04:27 +0300

    task_57

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,29 +1,20 @@
 import React, { useState } from 'react';
 
 function App() {
-  const [text1, setText1] = useState('');
-  const [text2, setText2] = useState('');
+  const [inputText, setInputText] = useState('');
 
-  function handleChangeInput1(event) {
-    setText1(event.target.value);
-  }
-
-  function handleChangeInput2(event) {
-    setText2(event.target.value);
+  function handleChange(event) {
+    const inputValue = event.target.value;
+    setInputText(inputValue);
   }
 
   return (
     <div>
-      <label>Input 1: </label>
-      <input value={text1} onChange={handleChangeInput1} />
-      <p>Text from Input 1: {text1}</p>
-
-      <label>Input 2: </label>
-      <input value={text2} onChange={handleChangeInput2} />
-      <p>Text from Input 2: {text2}</p>
+      <label>Input: </label>
+      <input value={inputText} onChange={handleChange} />
+      <p>Number of characters: {inputText.length}</p>
     </div>
   );
 }
 
 export default App;
-
