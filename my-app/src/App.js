b00dc4fa99af0b0b650382f7acead5f8e3e48ce1
SpartQS <rebrikov.vyacheslav@gmail.com>
2024-03-11 17:31:16 +0300

    task_62

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,63 +1,51 @@
-import React, { useState } from 'react';
-
-function App() {
-  const [text, setText] = useState('');
-
-  function handleChange(event) {
-    setText(event.target.value);
-  }
-
-  function transliterate(text) {
-    const transliterationMap = {
-      а: 'a', б: 'b', в: 'v', г: 'g', д: 'd', е: 'e', ё: 'e', ж: 'zh', з: 'z', и: 'i',
-      й: 'y', к: 'k', л: 'l', м: 'm', н: 'n', о: 'o', п: 'p', р: 'r', с: 's', т: 't',
-      у: 'u', ф: 'f', х: 'kh', ц: 'ts', ч: 'ch', ш: 'sh', щ: 'sch', ы: 'y', э: 'e',
-      ю: 'yu', я: 'ya', А: 'A', Б: 'B', В: 'V', Г: 'G', Д: 'D', Е: 'E', Ё: 'E', Ж: 'Zh',
-      З: 'Z', И: 'I', Й: 'Y', К: 'K', Л: 'L', М: 'M', Н: 'N', О: 'O', П: 'P', Р: 'R',
-      С: 'S', Т: 'T', У: 'U', Ф: 'F', Х: 'Kh', Ц: 'Ts', Ч: 'Ch', Ш: 'Sh', Щ: 'Sch',
-      Ы: 'Y', Э: 'E', Ю: 'Yu', Я: 'Ya'
-    };
-
-    return text.split('').map(char => transliterationMap[char] || char).join('');
-  }
-
-  const transliteratedText = transliterate(text);
-
-  return (
-    <div>
-      <textarea value={text} onChange={handleChange} />
-      <p>Transliteration: {transliteratedText}</p>
-    </div>
-  );
-}
-
-export default App;
-
 /*import React, { useState } from 'react';
 
 function App() {
-  const [numbers, setNumbers] = useState('');
+  const [isChecked, setChecked] = useState(false);
 
-  function handleChange(event) {
-    setNumbers(event.target.value);
-  }
-
-  function calculateSum() {
-    const sum = numbers.split('\n').reduce((acc, line) => {
-      const currentNumber = parseFloat(line);
-      return isNaN(currentNumber) ? acc : acc + currentNumber;
-    }, 0);
-
-    return sum;
+  function handleClick() {
+    alert(isChecked ? 'Привет, пользователь!' : 'До свидания!');
   }
 
   return (
     <div>
-      <textarea value={numbers} onChange={handleChange} />
-      <p>Sum of Numbers: {calculateSum()}</p>
+      <input type="checkbox" checked={isChecked} onChange={() => setChecked(!isChecked)} />
+      <button onClick={handleClick}>Показать приветствие/прощание</button>
+      <p>Состояние: {isChecked ? 'отмечен' : 'не отмечен'}</p>
     </div>
   );
 }
 
 export default App;*/
 
+import React, { useState } from 'react';
+
+function App() {
+  const [htmlChecked, setHtmlChecked] = useState(false);
+  const [cssChecked, setCssChecked] = useState(false);
+  const [jsChecked, setJsChecked] = useState(false);
+
+  return (
+    <div>
+      <label>
+        <input type="checkbox" checked={htmlChecked} onChange={() => setHtmlChecked(!htmlChecked)} />
+        HTML
+      </label>
+      <p>HTML: {htmlChecked ? 'знает' : 'не знает'}</p>
+
+      <label>
+        <input type="checkbox" checked={cssChecked} onChange={() => setCssChecked(!cssChecked)} />
+        CSS
+      </label>
+      <p>CSS: {cssChecked ? 'знает' : 'не знает'}</p>
+
+      <label>
+        <input type="checkbox" checked={jsChecked} onChange={() => setJsChecked(!jsChecked)} />
+        JavaScript
+      </label>
+      <p>JavaScript: {jsChecked ? 'знает' : 'не знает'}</p>
+    </div>
+  );
+}
+
+export default App;
