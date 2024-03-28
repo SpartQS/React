7f347701d6045dfd7ee967fc38802683742176c3
SpartQS <rebrikov.vyacheslav@gmail.com>
2024-03-28 18:50:24 +0300

    task_71

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,39 +1,53 @@
 import React, { useState } from 'react';
 
-function AverageArray() {
-    const [numbers, setNumbers] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+function App() {
+  const [date, setDate] = useState({
+      year: 2025,
+      month: 12,
+      day: 31
+  });
 
-    // Функция обработчик изменения элемента массива
-    function handleChange(index, event) {
-        const newValue = parseInt(event.target.value);
-        if (!isNaN(newValue)) { // Проверка, является ли ввод числом
-            setNumbers(prevNumbers => [
-                ...prevNumbers.slice(0, index),
-                newValue,
-                ...prevNumbers.slice(index + 1)
-            ]);
-        }
-    }
+  // Функция для изменения даты
+  function handleChange(prop, event) {
+      const newValue = parseInt(event.target.value);
+      if (!isNaN(newValue)) {
+          setDate(prevDate => ({
+              ...prevDate,
+              [prop]: newValue
+          }));
+      }
+  }
 
-    // Вычисление среднего арифметического
-    const average = numbers.reduce((acc, curr) => acc + curr, 0) / numbers.length;
+  // Функция для получения дня недели
+  function getDayOfWeek(year, month, day) {
+      const date = new Date(year, month - 1, day);
+      const options = { weekday: 'long' };
+      return new Intl.DateTimeFormat('en-US', options).format(date);
+  }
 
-    // Генерация компонентов инпутов для редактирования элементов массива
-    const inputs = numbers.map((number, index) => (
-        <input
-            key={index}
-            type="number"
-            value={number}
-            onChange={event => handleChange(index, event)}
-        />
-    ));
+  const { year, month, day } = date;
+  const dayOfWeek = getDayOfWeek(year, month, day);
 
-    return (
-        <div>
-            {inputs}
-            <p>Average: {average}</p>
-        </div>
-    );
+  return (
+      <div>
+          <input
+              type="number"
+              value={year}
+              onChange={event => handleChange('year', event)}
+          />
+          <input
+              type="number"
+              value={month}
+              onChange={event => handleChange('month', event)}
+          />
+          <input
+              type="number"
+              value={day}
+              onChange={event => handleChange('day', event)}
+          />
+          <p>Day of the week: {dayOfWeek}</p>
+      </div>
+  );
 }
 
-export default AverageArray;
+export default App;
